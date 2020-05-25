72c11c299e317c37ef81be7b69199a03d5f9993d
Alison Queiroz Soares <alison.soares@fugro.com>
2020-05-25 19:35:39 -0300

    sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,24 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NG_ENTITY_SERVICE_CONFIG } from '@datorama/akita-ng-entity-service';
 import { AkitaNgRouterStoreModule } from '@datorama/akita-ng-router-store';
+import { SidebarComponent } from './core/sidebar/sidebar.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './pages/login/login.component';
 import { ItemsComponent } from './pages/items/items.component';
 import { NgxAuthFirebaseUIModule } from 'ngx-auth-firebaseui';
 import { AkitaNgDevtools } from '@datorama/akita-ngdevtools';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { MatButtonModule } from '@angular/material/button';
 import { environment } from '../environments/environment';
 import { MaterialModule } from './Shared/material.module';
 import { AppRoutingModule } from './app-routing.module';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 import { AngularFireModule } from '@angular/fire';
 import { AppComponent } from './app.component';
-import { SidebarComponent } from './pages/sidebar/sidebar.component';
+import { LayoutModule } from '@angular/cdk/layout';
 
 @NgModule({
   declarations: [
@@ -34,7 +40,13 @@ import { SidebarComponent } from './pages/sidebar/sidebar.component';
     AkitaNgRouterStoreModule,
     environment.production ? [] : AkitaNgDevtools,
     NgxAuthFirebaseUIModule.forRoot(environment.firebase),
-    AngularFireModule.initializeApp(environment.firebase)
+    AngularFireModule.initializeApp(environment.firebase),
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [
     AngularFirestore,
---
+++ src/app/core/sidebar/sidebar.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable, of } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss']
+})
+export class SidebarComponent {
+
+  isHandset$: Observable<boolean> = of(true);
+
+  // this.breakpointObserver.observe(Breakpoints.Handset)
+  //   .pipe(
+  //     map(result => result.matches),
+  //     shareReplay()
+  //   );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+}
